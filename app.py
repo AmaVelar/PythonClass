94c0aa32ce298de6a3ee85a5c6c1dd9aa2e8de19
Yayra <yavprincess@gmail.com>
2020-11-19 10:11:59 +0000

    I love Python

---
+++ app.py
@@ -0,0 +1,50 @@
+print("Ama is learning Python")
+print("I am new with AWS")
+
+x=10
+print(type(x))
+
+y = "Is almost time!!"
+print(type(y))
+z=4.8
+a=True
+print(z)
+print(type(z))
+print(type(a))
+
+Firstnumber = 20
+Secondnumber = 200
+total = Firstnumber + Secondnumber
+print(total)
+
+# addition
+# subtraction
+# multiplication
+# division
+# modular
+
+num1 = 56
+num2 = 40
+mull = 56 * 40
+print(mull)
+
+num1 = 5
+num2 = 10
+null = 5 - 10
+print(null)
+
+a = 100
+b = 5
+c = 100 / 5
+print(c)
+
+# exponent
+# 2 ^ n - 1
+octect = (2 ** 7) - 1
+print(octect)
+
+# modulo
+dulo = 7 % 3
+print(dulo)
+
+
